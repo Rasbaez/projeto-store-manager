279057c69787fe5e352e0696f5b581d03bb84457
Roberto Baez <rbaezsp@gmail.com>
2022-11-20 21:35:29 -0300

    testando o avaliador

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -4,11 +4,11 @@ const productsRoute = Router();
 const productsController = require('../controllers/products.contoller');
 const { validateFields } = require('../middlewares/validateNewProductField');
 
-productsRoute.get('/search', productsController.getProductsByQuery);
 productsRoute.get('/', productsController.getProducts);
+productsRoute.get('/search', productsController.getProductsByQuery);
 productsRoute.get('/:id', productsController.getById);
 productsRoute.post('/', productsController.createProduct);
 productsRoute.put('/:id', validateFields, productsController.editProduct);
 productsRoute.delete('/:id', productsController.deleteProduct);
-
+// teste
 module.exports = productsRoute; 
